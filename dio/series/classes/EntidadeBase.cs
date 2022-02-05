a2d1c51ade2bb4792172655569b481dcd0652642
Luciano Gomes <66275836+lgomesroc@users.noreply.github.com>
2022-02-05 15:10:01 -0300

    Add files via upload

---
+++ dio/series/classes/EntidadeBase.cs
@@ -0,0 +1,15 @@
+namespace DIO.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}namespace DIO.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
